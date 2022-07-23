7dd6df523d329cbca01f2372db69c16bd5d7fa07
Adrian Arcega <adrianarcega18@gmail.com>
2022-07-24 05:34:33 +0800

    starting pdf design and data

--- Arcega Resume Creator/Form1.cs
+++ Arcega Resume Creator/Form1.cs
@@ -24,7 +24,8 @@ namespace Arcega_Resume_Creator
 
         public class resumecreator
         {
-            public string Fullname { get; set; }
+            public string firstname { get; set; }
+            public string lastname { get; set; }
             public string Profile { get; set; }
             public string Profile1 { get; set; }
             public string Profile2 { get; set; }
@@ -45,7 +46,7 @@ namespace Arcega_Resume_Creator
             public string skill7 { get; set; }
             public string skill8 { get; set; }
             public string Email { get; set; }
-            public string number { get; set; }
+            public string Number { get; set; }
             public string githubacc { get; set; }
             public string Address { get; set; }
             public string Birth { get; set; }
@@ -60,8 +61,49 @@ namespace Arcega_Resume_Creator
             }
             var jsonResumeCreator = JsonConvert.DeserializeObject<resumecreator>(jsonResume);
 
-            string Fullname = jsonResumeCreator.Fullname;
+            string firstname = jsonResumeCreator.firstname;
+            string lastname = jsonResumeCreator.lastname;
+            string Profile = jsonResumeCreator.Profile;
+            string Profile1 = jsonResumeCreator.Profile1;
+            string Profile2 = jsonResumeCreator.Profile2;
 
+            string Tertiary = jsonResumeCreator.Tertiary;
+            string Tertiary1 = jsonResumeCreator.Tertiary1;
+            string Secondary = jsonResumeCreator.Secondary;
+            string Secondary1 = jsonResumeCreator.Secondary1;
+            string Secondary2 = jsonResumeCreator.Secondary2;
+            string Secondary3 = jsonResumeCreator.Secondary3;
+            string Primary = jsonResumeCreator.Primary;
+
+            string skill = jsonResumeCreator.skill;
+            string skill1 = jsonResumeCreator.skill1;
+            string skill2 = jsonResumeCreator.skill2;
+            string skill3 = jsonResumeCreator.skill3;
+            string skill4 = jsonResumeCreator.skill4;
+            string skill5 = jsonResumeCreator.skill5;
+            string skill6 = jsonResumeCreator.skill6;
+            string skill7 = jsonResumeCreator.skill7;
+            string skill8 = jsonResumeCreator.skill8;
+
+            string Email = jsonResumeCreator.Email;
+            string Number = jsonResumeCreator.Number;
+            string githubacc = jsonResumeCreator.githubacc;
+            string Address = jsonResumeCreator.Address;
+            string Birth = jsonResumeCreator.Birth;
+            string Citizenship = jsonResumeCreator.Citizenship;
+
+         using (SaveFileDialog fileDialog = new SaveFileDialog())
+            {
+                fileDialog.InitialDirectory = @"C:\Users\acer\source\repos\Arcega Resume Creator\Arcega Resume Creator";
+                fileDialog.FileName = lastname + "" + firstname + ".pdf";
+                fileDialog.Filter = "PDF|*.pdf";
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    PdfDocument pdfDocument = new PdfDocument();
+                    pdfDocument.Info.Title = lastname + "_" + firstname + "Resume";
+                    PdfPage page = pdfDocument.AddPage(); 
+                }
+            }
 
         }
     }
